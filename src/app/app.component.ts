a66dd91ed1380efd53e8ff9d96e9865d60a3ef4e
cherites0610 <cherites0610@gmail.com>
2024-05-06 00:32:43 +0800

    feat(product-page): 新增產品清單頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,106 +1,14 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from './product-card/product-card.component';
-import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
 import { HeaderComponent } from './header/header.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardListComponent,HeaderComponent],
+  imports: [ProductPageComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  products = [
-    new Product({
-      id: 1,
-      name: '書籍 A',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 2,
-      name: '書籍 B',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      name: '書籍 C',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 4,
-      name: '書籍 D',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-  ];
-
-  onSetNoData(): void {
-    this.products = [];
-  }
-
-  onSetHasData(): void {
-    this.products = [
-      new Product({
-        id: 1,
-        name: '書籍 A',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '博碩文件',
-        isShow: true,
-        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date(),
-        price: 10000,
-      }),
-      new Product({
-        id: 2,
-        name: '書籍 B',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '博碩文件',
-        isShow: true,
-        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date(),
-        price: 10000,
-      }),
-      new Product({
-        id: 3,
-        name: '書籍 C',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '博碩文件',
-        isShow: true,
-        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date(),
-        price: 10000,
-      }),
-      new Product({
-        id: 4,
-        name: '書籍 D',
-        authors: ['作者甲', '作者乙', '作者丙'],
-        company: '博碩文件',
-        isShow: true,
-        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-        createDate: new Date(),
-        price: 10000,
-      }),
-    ];
-  }
 }
