d34be31114d4b801b5a0990aeaa67630c2afc71c
cherites0610 <cherites0610@gmail.com>
2024-05-06 00:37:47 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Product } from './model/product';
 import { HeaderComponent } from './header/header.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductPageComponent,HeaderComponent],
+  imports: [RouterOutlet,ProductPageComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
