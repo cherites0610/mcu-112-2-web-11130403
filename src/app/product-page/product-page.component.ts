97f510470fc8243484d9ea4f211f96b64f2b32cc
cherites0610 <cherites0610@gmail.com>
2024-05-14 11:13:43 +0800

    feat(service): 從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Output } from '@angular/core';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../model/product';
 import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -11,50 +12,16 @@ import { Router } from '@angular/router';
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
-  products = [
-    new Product({
-      id: 1,
-      name: '書籍 A',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 2,
-      name: '書籍 B',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      name: '書籍 C',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 4,
-      name: '書籍 D',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-  ];
-
   router = inject(Router)
+
+  private productService!: ProductService;
+
+  products!: Product[];
+
+  ngOnInit(): void {
+    this.productService = new ProductService();
+    this.products = this.productService.getDate();
+  }
 
   @Output()
   onView(product: Product): void {
