99e6fb347b894e3dad6b5b3daa96c1237404a3cf
cherites0610 <cherites0610@gmail.com>
2024-05-29 23:37:41 +0800

    feat(product): 實作後端服務取得所有產品資料

---
+++ src/app/services/product-remote.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from "@angular/core";
+import { ProductService } from "./product.service";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Product } from "../model/product";
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class ProductRemoteService extends ProductService {
+    private readonly url = 'http://localhost:3000/products';
+
+    private readonly httpClient = inject(HttpClient)
+
+    override getDate(): Observable<Product[]> {
+        return this.httpClient.get<Product[]>(this.url);
+    }
+}
