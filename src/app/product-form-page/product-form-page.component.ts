845860252516bec25ae2bb4fd667a108c9fdfe87
cherites0610 <cherites0610@gmail.com>
2024-06-19 17:47:09 +0800

    feat(product-form): 產品表單驗證實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,42 +1,55 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
-import { Product } from '../model/product';
 import { map } from 'rxjs';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { IProductForm } from './product-form.interface';
+
+import { Product } from '../model/product';
+import { IProductForm } from '../interface/product-form.interface';
 
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
-  styleUrl: './product-form-page.component.css'
+  styleUrl: './product-form-page.component.css',
 })
-export class ProductFormPageComponent {
+export class ProductFormPageComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
-
-  product !: Product;
 
   form = new FormGroup<IProductForm>({
     id: new FormControl<number | null>(null),
-    name: new FormControl<string | null>(null),
-    company: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     authors: new FormArray<FormControl<string | null>>([]),
+    company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
-    price: new FormControl<string | null>(null),
+    price: new FormControl<string | null>(null, { validators: [Validators.required, Validators.pattern('^[0-9]$')] }),
   });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
 
   get authors(): FormArray<FormControl<string | null>> {
     return this.form.get('authors') as FormArray<FormControl<string | null>>;
   }
 
-  onAddAuthors(): void {
-    const formControl = new FormControl<string | null>(null);
-    this.authors.push(formControl);
+  get company(): FormControl<string | null> {
+    return this.form.get('company') as FormControl<string | null>;
   }
+
+  get price(): FormControl<string | null> {
+    return this.form.get('price') as FormControl<string | null>;
+  }
+
+  product!: Product;
 
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthors(): void {
+    const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
+    this.authors.push(formControl);
   }
 }
